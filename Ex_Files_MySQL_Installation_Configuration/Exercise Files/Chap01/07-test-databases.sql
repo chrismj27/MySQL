3969296820e66211d8d54922a6bb27fe9409acb6
chrismj27 <chrismj27@gmail.com>
2023-01-06 23:18:21 -0500

    ..

---
+++ Ex_Files_MySQL_Installation_Configuration/Exercise Files/Chap01/07-test-databases.sql
@@ -0,0 +1,23 @@
+-- test installed databases
+
+USE world;
+SHOW tables;
+
+USE scratch;
+SHOW tables;
+
+USE album;
+SHOW tables;
+
+USE album;
+SELECT * FROM album;
+
+LG = Lawful Good
+NG = Neutral Good
+CG = Chaotic Good
+LN = Lawful Neutral
+TN = True Neutral
+CN = Chaotic Neutral
+LE = Lawful Evil
+NE = Neutral Evil
+CE = Chaotic Evil 
